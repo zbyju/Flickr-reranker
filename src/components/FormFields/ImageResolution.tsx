3b76fd1aa483463e13978c6c715f19b8f81d336d
zbyju <zbynek.jurica@gmail.com>
2021-10-21 20:45:39 +0200

    Change resolution inputs to numerical

--- src/components/FormFields/ImageResolution.tsx
+++ src/components/FormFields/ImageResolution.tsx
@@ -1,12 +1,24 @@
-import { Heading, Input } from "@chakra-ui/react";
+import { Heading, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper} from "@chakra-ui/react";
 import WeightSlider from "./WeightSlider";
 
 const ImageResolution = () => {
     return (
         <>
             <Heading size="sm" alignSelf="flex-start">Resolution</Heading>
-            <Input placeholder="Image width" />
-            <Input placeholder="Image height" />
+            <NumberInput w="100%" step={10} min={0} max={20000}>
+                <NumberInputField placeholder="Image width"/>
+                <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                </NumberInputStepper>
+            </NumberInput>
+            <NumberInput w="100%" step={10} min={0} max={20000}>
+                <NumberInputField placeholder="Image height"/>
+                <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                </NumberInputStepper>
+            </NumberInput>
             <WeightSlider />
         </>
     );
