a60bb4dec19c9d42ce7e56501d4f45cd0303f813
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-11-27 20:20:09 +0100

    Fix layout overflow

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ function App() {
   return (
     <div className="App">
       <ChakraProvider>
-        <HStack minH="100vh" height="100%" minW="100vw" h="1" spacing={0}>
+        <HStack minH="100vh" height="100%" w="100%" minW="100vw" h="1" spacing={0} overflow-x="hidden">
           <SearchForm />
-          <SimpleGrid columns={2} w="100%" h="100%">
+          <SimpleGrid columns={2} w="100%" h="100%" overflow-x="hidden">
             <ResultList />
             <ResultListReranked />
           </SimpleGrid>
