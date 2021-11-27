dbf09fdf857cea6760c50c75a56da53f4139166c
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-11-27 23:03:33 +0100

    Fix spacing

--- src/components/ImageRank.tsx
+++ src/components/ImageRank.tsx
@@ -25,11 +25,11 @@ const ImageRank = ({ title, newRank, oldRank }: ImageRankProps) => {
       <Text>
         <b>{title} </b>
         ({
-          diff > 0 ? (<><ChevronUpIcon p="0" mx="-1" pb="2px" w={7} h={8} color="green.500" /><Text as="span">+{diffStr}</Text></>)
+          diff > 0 ? (<><ChevronUpIcon p="0" mx="-1" pb="2px" mb="-2px" w={7} h={8} color="green.500" /><Text as="span">+{diffStr}</Text></>)
             :
-            diff < 0 ? (<><ChevronDownIcon p="0" mx="-1" pb="2px" w={7} h={8} color="red.500" /><Text as="span">-{diffStr}</Text></>)
+            diff < 0 ? (<><ChevronDownIcon p="0" mx="-1" pb="2px" mb="-2px" w={7} h={8} color="red.500" /><Text as="span">-{diffStr}</Text></>)
               :
-              (<><MinusIcon p="0" pb="2px" w={5} h={8} color="blue.500" /><Text as="span"></Text></>)
+              (<><MinusIcon p="0" pb="2px" w={5} h={8} color="blue.500" mb="-2px" /><Text as="span"></Text></>)
         })
         <b>: </b>{newRank}
       </Text>
