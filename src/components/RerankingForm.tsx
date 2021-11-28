eaea69c22c8c4418646b42adfe9b25e23f46d604
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-11-28 15:32:25 +0100

    Make apply button clickable only when something changed

--- src/components/RerankingForm.tsx
+++ src/components/RerankingForm.tsx
@@ -1,8 +1,9 @@
 import { Button, Heading } from "@chakra-ui/react";
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getDefaultDateTakenField, getDefaultGPSField, getDefaultResolutionField, getDefaultTitleField } from "../factory/formFactory";
 import { updateRerankForm } from "../redux/actions/rerankFormActions";
+import { RootState } from "../redux/reducers";
 import { DateTakenField, GPSField, ResolutionField, TitleField } from "../types/fields";
 import Date from "./FormFields/Date";
 import GPS from "./FormFields/GPS";
@@ -11,11 +12,24 @@ import Title from "./FormFields/Title";
 
 const RerankingForm = () => {
     const dispatch = useDispatch()
+    const rerankForm = useSelector((state: RootState) => state.rerankForm)
     const [titleField, setTitleField] = useState<TitleField>(getDefaultTitleField())
     const [resolutionField, setResolutionField] = useState<ResolutionField>(getDefaultResolutionField())
     const [dateTakenField, setDateTakenField] = useState<DateTakenField>(getDefaultDateTakenField())
     const [gpsField, setGpsField] = useState<GPSField>(getDefaultGPSField())
 
+    const isFormChanged = () => {
+        return (
+            titleField.data !== rerankForm.titleField.data ||
+            titleField.weight !== rerankForm.titleField.weight ||
+            resolutionField.data !== rerankForm.resolutionField.data ||
+            resolutionField.weight !== rerankForm.resolutionField.weight ||
+            dateTakenField.data !== rerankForm.dateTakenField.data ||
+            dateTakenField.weight !== rerankForm.dateTakenField.weight ||
+            gpsField.data !== rerankForm.gpsField.data ||
+            gpsField.weight !== rerankForm.gpsField.weight
+        )
+    }
 
     const applyClicked = () => {
         dispatch(updateRerankForm({
@@ -36,7 +50,7 @@ const RerankingForm = () => {
             <GPS value={gpsField} setValue={setGpsField} />
 
 
-            <Button colorScheme="blue" onClick={applyClicked}>Apply</Button>
+            <Button isDisabled={!isFormChanged()} colorScheme="blue" onClick={applyClicked}>Apply</Button>
         </>
     );
 }
