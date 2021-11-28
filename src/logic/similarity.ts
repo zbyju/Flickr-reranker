3215c4c8d1d5d575dd2d46def34e5e8427f09256
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-11-28 15:32:06 +0100

    Change levenshtein distance to use dynamic programming solution

--- src/logic/similarity.ts
+++ src/logic/similarity.ts
@@ -1,14 +1,26 @@
-import moment from "moment"
+import moment from "moment";
 
-const levenshteinDistance = (s1: string, s2: string): number => {
-  if(s1.length === 0) return s2.length
-  if(s2.length === 0) return s1.length
-  if(s1[0] === s2[0]) return levenshteinDistance(s1.substring(1), s2.substring(1))
-  return 1 + Math.min(
-      levenshteinDistance(s1.substring(1), s2),
-      levenshteinDistance(s1, s2.substring(1)),
-      levenshteinDistance(s1.substring(1), s2.substring(1))
-    )
+// Code used from https://www.tutorialspoint.com/levenshtein-distance-in-javascript
+const levenshteinDistance = (str1: string, str2: string): number => {
+  const track = Array(str2.length + 1).fill(null).map(() =>
+  Array(str1.length + 1).fill(null));
+  for (let i = 0; i <= str1.length; i += 1) {
+      track[0][i] = i;
+  }
+  for (let j = 0; j <= str2.length; j += 1) {
+      track[j][0] = j;
+  }
+  for (let j = 1; j <= str2.length; j += 1) {
+      for (let i = 1; i <= str1.length; i += 1) {
+        const indicator = str1[i - 1] === str2[j - 1] ? 0 : 1;
+        track[j][i] = Math.min(
+            track[j][i - 1] + 1, // deletion
+            track[j - 1][i] + 1, // insertion
+            track[j - 1][i - 1] + indicator, // substitution
+        );
+      }
+  }
+  return track[str2.length][str1.length];
 }
 
 export const levenshteinDistanceNormalized = (s1: string, s2: string): number => {
