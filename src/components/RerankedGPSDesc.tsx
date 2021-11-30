41502b0bf8bc272f0898ff8c91d7e7d24a0a30c5
zbyju <zbynek.jurica@gmail.com>
2021-11-30 23:45:17 +0100

    Add show on map button in description

---
+++ src/components/RerankedGPSDesc.tsx
@@ -0,0 +1,57 @@
+import { Box, Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, VStack, useDisclosure } from "@chakra-ui/react";
+import { useState } from "react";
+import { GPSLocation } from "../types/fields";
+import GPSModalContent from "./formFields/GPSModalContent";
+
+interface RerankedGPSDescProps {
+  score?: number
+  gps: GPSLocation
+}
+
+const RerankedGPSDesc = ({ score, gps }: RerankedGPSDescProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const tmpLocation = useState(gps)
+  return (
+    <>
+      <Box
+        px={2} py={1}
+        w="100%"
+        borderBottom="1px"
+        borderColor="blue.100"
+        style={{ borderCollapse: "collapse" }}
+        _hover={{
+          background: "rgb(225, 238, 250)"
+        }}
+        transition="0.3s all"
+      >
+        <VStack>
+          <Text>
+            <b>GPS</b>
+            {score !== undefined && <i> (Score: {score.toFixed(2)})</i>}
+            <b>: </b>
+            <span>{gps.lat}, {gps.lng}</span>
+          </Text >
+          <Button size="xs" alignSelf="center" colorScheme="green" onClick={onOpen}>Show on map</Button>
+        </VStack>
+      </Box>
+      <Modal isOpen={isOpen} onClose={onClose} size="full">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Pick location</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <GPSModalContent locationState={tmpLocation} locationChangable={false} />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="red" onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+export default RerankedGPSDesc;
